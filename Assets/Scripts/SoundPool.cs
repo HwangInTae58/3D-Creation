1e5742ce5ebf4986fd27864bd5572f8e11875921
HwangInTae58 <99638031+HwangInTae58@users.noreply.github.com>
2022-07-04 21:15:53 +0900

    GameTitle set
    GameTitle set,
    SoundPool Creating

---
+++ Assets/Scripts/SoundPool.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPool : MonoBehaviour
+{
+    public GameObject soundSource;
+    public AudioSource[] prefab;
+
+    int soundNum = 0;
+    int maxSoundPrefab = 100;
+    private void Start()
+    {
+        for(int i = 0; i < maxSoundPrefab; i++) {
+            prefab[i] =  Instantiate(prefab[i]);
+            prefab[i].transform.SetParent(soundSource.transform, false);
+            prefab[i].enabled = false;
+        }
+    }
+    public void SetSound(AudioClip _clip, Transform _position, float audioEnd)
+    {
+        prefab[soundNum].enabled = true;
+        prefab[soundNum].clip = _clip;
+        prefab[soundNum].transform.position = _position.position;
+        
+        if(prefab[soundNum].time>= audioEnd)
+        {
+            prefab[soundNum].enabled = false;
+        }
+
+        soundNum++;
+        if(soundNum >= maxSoundPrefab)
+        {
+            soundNum = 0;
+        }
+    }
+}
